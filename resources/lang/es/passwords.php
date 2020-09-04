1363676fabf67742e16dae1383947e4f34a5e83e
mzevallosv <62776313+mzevallosv@users.noreply.github.com>
2020-09-04 00:34:57 -0500

    Cambios menores

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => 'Su contraseña ha sido reseteada.',
+    'sent' => 'se le envió un link para el reseteo de su contraseña',
+    'throttled' => 'Por favor espere antes de intentar nuevamente',
+    'token' => 'El token de reseteo es incorrecto.',
+    'user' => "No se pudo encontrar al usuario con este email.",
+
+];
